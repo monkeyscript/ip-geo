7090217d923692f833ae79bb977e56926906876f
MonkeyScript <monkeyscript.dev@gmail.com>
2020-10-02 10:50:24 +0530

    add basic functionality

---
+++ main.js
@@ -0,0 +1,28 @@
+function track() {
+    
+    // Get value from input 
+    let ip = document.querySelector('#ipinput').value;
+    console.log(ip);
+    
+    // Validate ip address 
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)){
+        
+        // Ipify geo locator
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", 'https://geo.ipify.org/api/v1?apiKey=at_0WNtZRbJTqnWs0BlbvHvREcQW7Aej');
+        xhttp.send();
+
+        xhttp.onreadystatechange = () => {
+            if(xhttp.responseText!='') {
+
+                // Show output 
+                document.querySelector('#ipoutput').textContent = JSON.stringify(JSON.parse(xhttp.responseText), undefined, 2);
+                
+            }
+        }
+        
+    }else {
+        alert('Invalid IP address :(')
+    }
+    
+}
