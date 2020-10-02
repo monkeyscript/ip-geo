58622fe317fe74b1a02768d0fd82eb60222f4ee4
MonkeyScript <monkeyscript.dev@gmail.com>
2020-10-02 11:43:34 +0530

    Update CSS

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function track() {
     // Validate ip address 
     if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)){
         
+        document.querySelector('pre').style.display = 'block';
+
         // Ipify geo locator
         var xhttp = new XMLHttpRequest();
         xhttp.open("GET", 'https://geo.ipify.org/api/v1?apiKey=at_0WNtZRbJTqnWs0BlbvHvREcQW7Aej');
